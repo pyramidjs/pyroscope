907b28c39ac8fd260a351e7702eb6f914f098137
Sean Cheung <theoxuanx@gmail.com>
2018-01-05 01:59:09 +0800

    renderer update

--- framework/renderer.js
+++ framework/renderer.js
@@ -22,71 +22,79 @@ function unescape(val) {
     });
 }
 
-function compile(html, context) {
-    const $ = cheerio.load(html);
-
-    $('*[ps-if]').each((i, e) => {
-        let res;
+function replace(text, context) {
+    return text.replace(/\{\{([^{}]+)\}\}/g, (match, capture) => {
         try {
-            res = vm.runInNewContext($(e).attr('ps-if'), context);
-        } catch (err) {
-            debug(err);
-            res = false;
-        }
-        if (!res) {
-            $(e)
-                .next('*[ps-else]')
-                .removeAttr('ps-else');
-            $(e).remove();
-        } else {
-            $(e)
-                .next('*[ps-else]')
-                .remove();
-            $(e).removeAttr('ps-if');
-        }
-    });
-
-    $('*[ps-for]').each((i, e) => {
-        const val = $(e).attr('ps-for');
-        const args = val.split(/\s+/);
-        if (args.length === 3 && /^(in|of)$/.test(args[1])) {
-            const name = args[0];
-            const exp = args[2];
-            let res;
-            try {
-                res = vm.runInNewContext(exp, context);
-            } catch (err) {
-                debug(err);
-                res = undefined;
-            }
-            if (Array.isArray(res)) {
-                const temp = $(e).html();
-                $(e)
-                    .children()
-                    .remove();
-                res.forEach((v, i) => {
-                    $(e).append(
-                        compile(temp, {
-                            [name]: v,
-                            $index: i,
-                            $parent: context
-                        })
-                    );
-                });
-                $(e).removeAttr('ps-for');
-            }
-        }
-    });
-
-    return $.html().replace(/\{\{([^{}]+)\}\}/g, (match, capture) => {
-        try {
-            return vm.runInNewContext(unescape(capture), context);
+            return vm.runInContext(unescape(capture), context);
         } catch (err) {
             debug(err);
 
             return match;
         }
     });
+}
+
+function compile(html, options) {
+    const dom = cheerio.load(html);
+
+    return traverse(dom.root(), options).html();
+}
+
+function traverse(html, options) {
+    const context = vm.createContext(options);
+    const $ = cheerio(html);
+    if ($.attr('ps-if')) {
+        try {
+            const res = vm.runInContext($.attr('ps-if'), context);
+            if (!res) {
+                $.remove();
+                $.next('*[ps-else]').removeAttr('ps-else');
+
+                return $;
+            }
+            $.removeAttr('ps-if');
+            $.next('*[ps-else]').remove();
+        } catch (error) {
+            debug(error);
+        }
+    }
+    if ($.attr('ps-for')) {
+        const val = $.attr('ps-for');
+        const args = val.split(/\s+/);
+        if (args.length === 3 && /^(in|of)$/.test(args[1])) {
+            const name = args[0];
+            const exp = args[2];
+            try {
+                const res = vm.runInContext(exp, context);
+                if (Array.isArray(res)) {
+                    const temp = $.html();
+                    $.children().remove();
+                    res.forEach((v, i) => {
+                        const child = traverse(temp, {
+                            [name]: v,
+                            $index: i,
+                            $parent: options
+                        });
+                        $.append(child);
+                    });
+                    $.removeAttr('ps-for');
+                }
+            } catch (error) {
+                debug(error);
+            }
+        }
+    } else {
+        $.children().each((i, e) => {
+            cheerio(e).html(traverse(e, options).html());
+        });
+    }
+
+    // if ($.html()) {
+    //     $.html(replace($.html(), context));
+    // }
+    //TODO: li
+
+    return $;
 }
 
 module.exports = {
